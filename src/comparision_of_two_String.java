b83e4969f2ae5c5681161155d3f44046e7492a7e
Rishitkesharwani <rishitkesharwani@gmail.com>
2024-01-31 23:12:37 +0530

    comparishion of two String with lexicographic Order

---
+++ src/comparision_of_two_String.java
@@ -0,0 +1,12 @@
+public class comparision_of_two_String {
+    public static void main(String[] args) {
+        String[] fruit= {"apple","banana","mango"};
+        String largest=fruit[0];
+        for (int i = 1; i < fruit.length ; i++) {
+            if(largest.compareTo(fruit[i])<0){
+                largest=fruit[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
