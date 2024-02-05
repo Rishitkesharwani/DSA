87cca75deed9897343470083766bf811967f1cf8
Rishitkesharwani <rishitkesharwani@gmail.com>
2024-02-05 23:16:58 +0530

    factorial of a number using recursion

---
+++ src/recursion/fact_of_num.java
@@ -0,0 +1,17 @@
+package recursion;
+
+public class fact_of_num {
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+       int fcn1= fact(n-1);
+        int fac=(n*fcn1);
+        return fac;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(fact(n));
+
+    }
+}
