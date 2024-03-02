856b47bf5c507b561e95093945f604f2b4d34e73
Rishitkesharwani <rishitkesharwani@gmail.com>
2024-03-03 00:49:30 +0530

    Deletion in DLL at first in position in linked list

---
+++ src/linked_list/DLL_deletion_at_Last.java
@@ -0,0 +1,107 @@
+package linked_list;
+
+public class DLL_deletion_at_Last {
+    public static class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public static void addFirst(int data) {
+        Node newnode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newnode;
+            return;
+        }
+        newnode.next = head;
+        head.prev = newnode;
+        head = newnode;
+    }
+
+    public static void addLast(int data) {
+        Node newnode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newnode;
+            return;
+        }
+        tail.next = newnode;
+        newnode.prev = tail;
+        tail = newnode;
+    }
+
+    public static int delFirst() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return -1;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public static int delLast() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return -1;
+        }
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+
+        size--;
+        return val;
+    }
+
+    public static void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        addFirst(4);
+        addFirst(2);
+        addFirst(5);
+        print();
+
+        addLast(3);
+        addLast(6);
+        print();
+
+        delFirst();
+        print();
+        delLast();
+        print();
+        System.out.println(size);
+    }
+}
