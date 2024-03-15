54aa0fe7a2589386e64a60e3afc7d2d2d50eead5
Rishitkesharwani <rishitkesharwani@gmail.com>
2024-03-15 14:44:10 +0530

    Insertion short

---
+++ src/insertion_short.java
@@ -0,0 +1,17 @@
+public class insertion_short {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        for(int i=1;i<arr.length;i++){
+            int top=i-1;
+            int new_card=arr[i];
+            while(top>=0 && arr[top]>new_card){
+                arr[top+1]=arr[top];
+                top--;
+            }
+            arr[top+1]=new_card;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
